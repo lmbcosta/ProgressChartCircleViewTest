26f1168fa368cdd273748bd19cc7353c3d8f7469
lmbcosta <lmbcosta@hotmail.com>
2018-02-27 00:36:52 +0000

    Fixing percentage

--- ProgressChartCircleViewTest/ViewController.swift
+++ ProgressChartCircleViewTest/ViewController.swift
@@ -13,20 +13,20 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var circleChart: ProgressChartCircleView!
     var index: Int = -1
-//    var models = [
-//        ProgressCircleChartModel(id: 1, startPercentage: 0, endPercentage: 72, color: .green),
-//        ProgressCircleChartModel(id: 2, startPercentage: 72, endPercentage: 80, color: .red),
-//        ProgressCircleChartModel(id: 3, startPercentage: 80, endPercentage: 83, color: .black),
-//        ProgressCircleChartModel(id: 4, startPercentage: 83, endPercentage: 89, color: .red),
-//        ProgressCircleChartModel(id: 5, startPercentage: 89, endPercentage: 93, color: .orange),
-//        ProgressCircleChartModel(id: 6, startPercentage: 97, endPercentage: 100, color: .yellow)
-//    ]
-    
     var models = [
-        ProgressCircleChartModel(id: 1, startPercentage: 0, endPercentage: 54, color: .green),
-        ProgressCircleChartModel(id: 2, startPercentage: 54, endPercentage: 72, color: .orange),
-        ProgressCircleChartModel(id: 2, startPercentage: 72, endPercentage: 100, color: .red),
+        ProgressCircleChartModel(id: 1, startPercentage: 0, endPercentage: 72, color: .green),
+        ProgressCircleChartModel(id: 2, startPercentage: 72, endPercentage: 80, color: .red),
+        ProgressCircleChartModel(id: 3, startPercentage: 80, endPercentage: 83, color: .black),
+        ProgressCircleChartModel(id: 4, startPercentage: 83, endPercentage: 89, color: .red),
+        ProgressCircleChartModel(id: 5, startPercentage: 89, endPercentage: 93, color: .orange),
+        ProgressCircleChartModel(id: 6, startPercentage: 93, endPercentage: 100, color: .yellow)
     ]
+    
+//    var models = [
+//        ProgressCircleChartModel(id: 1, startPercentage: 0, endPercentage: 54, color: .green),
+//        ProgressCircleChartModel(id: 2, startPercentage: 54, endPercentage: 72, color: .orange),
+//        ProgressCircleChartModel(id: 2, startPercentage: 72, endPercentage: 100, color: .red),
+//    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,8 +35,8 @@ class ViewController: UIViewController {
         circleChart.selectedStrokeColor = .green
         circleChart.backgroundStrokeColor = .lightGray
         circleChart.setModels(models: models)
-        circleChart.labelFont = UIFont.systemFont(ofSize: 42, weight: .medium)
-        circleChart.labelSize = CGSize(width: 90, height: 64)
+        circleChart.labelFont = UIFont.systemFont(ofSize: 52, weight: .medium)
+        circleChart.labelSize = CGSize(width: 120, height: 66)
     }
     
     @IBAction func buttonTapped(_ sender: Any) {
